5fb8a1691312a0300323c6d9c678317e43e04140
JoaoF2025 <joaolmf@ua.pt>
2022-10-10 02:51:36 +0100

    Latest changes

--- PSR_Solutions/Aula04/TestV1.py
+++ PSR_Solutions/Aula04/TestV1.py
@@ -9,21 +9,31 @@ import random
 
 def printChars(stop_char):
     myList=[]
+    randomList=[]
+    print('Type something (Press ' + colorama.Fore.RED + stop_char + colorama.Style.RESET_ALL + ' to stop)')
     while True:
-        print('Type something (Press ' + colorama.Fore.RED + stop_char + colorama.Style.RESET_ALL + ' to stop)')
+        key=chr(random.randint(97,122))
+        print('Press the key ' + key)
         pressed_key=readchar.readkey()
-        
+
         if pressed_key==stop_char:
             print('You typed ' + colorama.Fore.RED + stop_char + colorama.Style.RESET_ALL + '... terminating process... Cyaaaaaa') 
             break
-        else:
+        if pressed_key==key:
             myList.append(pressed_key)
-            key=random.randint(97,122)
-            print('Your random number is '+ str(key) + ' and it\'s equivalent to '+ str(chr(key)))
-            print('Key ' + colorama.Fore.YELLOW + pressed_key + colorama.Style.RESET_ALL + ' was added to your list.')
-    
-    print('Thank you for typing, here\'s your list: ' + str(myList))
+            randomList.append(key)
+            print('Congrats!')
+        else:
+            randomList.append(key)
+            print("Try Again!")
 
+    mlNum=len(myList)
+    rdmNum=len(randomList)
+    acc=int((mlNum/rdmNum)*100)
+    if mlNum==rdmNum:
+        print('Congrats! You got ' + colorama.Fore.RED + str(acc) +'%' + colorama.Style.RESET_ALL + ' accuracy! Well Done <3')
+    else:
+        print('Ufff, almost there! You got '+ colorama.Fore.RED + str(acc) +'%' +colorama.Style.RESET_ALL+' accuracy! Keep Trying <3')
 
        
 def main():
